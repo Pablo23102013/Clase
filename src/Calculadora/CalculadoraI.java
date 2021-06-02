95092b143e3a0dfe600ab9fbe98287519e57d2d1
Pablo <pablo23my@gmail.com>
2021-06-02 09:33:32 +0200

    k

--- src/Calculadora/CalculadoraI.java
+++ src/Calculadora/CalculadoraI.java
@@ -87,6 +87,7 @@ private JLabel Etiqueta2;
               int resultado=valor1+valor2;
               String txtresultado = String.valueOf(resultado);
               TResutado.setText(txtresultado);
+              System.out.println("");
               
           }
       });
